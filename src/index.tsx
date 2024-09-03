99c9d8cec4f0e6e205ba5361b0bac568a20027bf
andre2xu <>
2024-09-03 11:12:23 +0100

    Set up a router and added a route for the home page.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+
+// views
+import Home from './views/home';
 
 
+
+const ROUTER = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+]);
 
 const ROOT: ReactDOM.Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 ROOT.render(
   <React.StrictMode>
-
+    <RouterProvider router={ROUTER} />
   </React.StrictMode>
 );
