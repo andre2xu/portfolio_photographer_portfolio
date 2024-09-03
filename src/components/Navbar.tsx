d5178d91ebdbbbdd2c50cc7b00d375deb86ddabf
andre2xu <>
2024-09-03 13:45:08 +0100

    Added a hamburger menu to the navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,6 +14,19 @@ function Navbar() {
                 <li><Link to={'/'}>About Me</Link></li>
                 <li><Link to={'/'}>Contact</Link></li>
             </ul>
+
+            <div className='hamburger-menu-button'>
+                <div className='hamburger-menu-line'></div>
+                <div className='hamburger-menu-line'></div>
+                <div className='hamburger-menu-line'></div>
+            </div>
+
+            <ul className='hamburger-menu'>
+                <li>X</li>
+                <li><Link to={'/'}>My Work</Link></li>
+                <li><Link to={'/'}>About Me</Link></li>
+                <li><Link to={'/'}>Contact</Link></li>
+            </ul>
         </nav>
     );
 };
