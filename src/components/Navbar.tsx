b078d380f819a7130ec6b267b362dde586adbb54
andre2xu <>
2024-09-03 14:53:54 +0100

    The navbar's hamburger menu can now be displayed and hidden.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,45 @@
+import $ from 'jquery';
 import { Link } from "react-router-dom";
 
 
 
 function Navbar() {
+    // HELPERS
+    function toggleHamburgerMenu() {
+        const HAMBURGER_MENU: JQuery<HTMLElement> = $('nav .hamburger-menu').first();
+
+        if (HAMBURGER_MENU.hasClass('hide')) {
+            HAMBURGER_MENU.removeClass('hide');
+
+            $({pos: -100}).animate(
+                {pos: 0},
+                {
+                    duration: 500,
+                    step: (now) => {
+                        HAMBURGER_MENU.css({right: `${now}%`});
+                    }
+                }
+            );
+        }
+        else {
+            $({pos: 0}).animate(
+                {pos: -100},
+                {
+                    duration: 500,
+                    step: (now) => {
+                        HAMBURGER_MENU.css({right: `${now}%`});
+                    },
+                    complete: () => {
+                        HAMBURGER_MENU.addClass('hide');
+                    }
+                }
+            );
+        }
+    };
+
+
+
+    // HTML
     return (
         <nav>
             <h1>
@@ -15,14 +52,14 @@ function Navbar() {
                 <li><Link to={'/'}>Contact</Link></li>
             </ul>
 
-            <div className='hamburger-menu-button'>
+            <div className='hamburger-menu-button' onClick={toggleHamburgerMenu}>
                 <div className='hamburger-menu-line'></div>
                 <div className='hamburger-menu-line'></div>
                 <div className='hamburger-menu-line'></div>
             </div>
 
             <ul className='hamburger-menu hide'>
-                <li>X</li>
+                <li onClick={toggleHamburgerMenu}>X</li>
                 <li><Link to={'/'}>My Work</Link></li>
                 <li><Link to={'/'}>About Me</Link></li>
                 <li><Link to={'/'}>Contact</Link></li>
