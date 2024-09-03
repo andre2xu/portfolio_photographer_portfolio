4e5c0f6b9cefb5ededb2083e394d357e4858cebe
andre2xu <>
2024-09-03 21:35:46 +0100

    Created HTML for the home page's teaser section.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,38 @@
-import Navbar from "../components/Navbar";
+// shared components
+import Navbar from '../components/Navbar';
 
 
 
 function Home() {
     return (
-        <div id="home-page">
+        <div id='home-page'>
             <Navbar />
+
+            <main>
+                <div id='teaser-section' className='section'>
+                    <div>
+                        <h1>Professional Photos</h1>
+
+                        <p>Don't settle for less. Go for the best.</p>
+                    </div>
+
+                    <div id='camera-shutter'>
+                        <div id='lens'></div>
+
+                        <div className='cover n3'></div>
+                        <div className='cover n2'></div>
+                        <div className='cover n1'></div>
+                        <div className='cover n6'></div>
+                        <div className='cover n5'></div>
+                        <div className='cover n4'></div>
+
+                        <div className='teaser-image hide' style={{backgroundImage: 'url(/images/pexels-misha-earle-366060-1777843.jpg)'}}></div>
+                        <div className='teaser-image hide' style={{backgroundImage: 'url(/images/pexels-sabel-blanco-662810-2339724.jpg)'}}></div>
+                        <div className='teaser-image hide' style={{backgroundImage: 'url(/images/pexels-kpaukshtite-3270222.jpg)'}}></div>
+                        <div className='teaser-image' style={{backgroundImage: 'url(/images/pexels-pixabay-258154.jpg)'}}></div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
