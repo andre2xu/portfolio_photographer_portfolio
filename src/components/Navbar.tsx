2b371f13b6676ac7674e9efdadace2dfb86f49f2
andre2xu <>
2024-09-03 11:46:24 +0100

    Created HTML for the navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
+
+
 function Navbar() {
     return (
         <nav>
-            
+            <h1><Link to={'/'}>Protos</Link></h1>
+
+            <ul className='navbar-links'>
+                <li><Link to={'/'}>My Work</Link></li>
+                <li><Link to={'/'}>About Me</Link></li>
+                <li><Link to={'/'}>Contact</Link></li>
+            </ul>
         </nav>
     );
 };
