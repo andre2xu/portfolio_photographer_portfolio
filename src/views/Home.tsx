56d453ad7efce62c5be01c0a8a43ff6ffd126480
andre2xu <>
2024-09-04 13:27:13 +0100

    Created a helper for opening and closing the camera shutter.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,107 @@
+import React from 'react';
+import $ from 'jquery';
+
 // shared components
 import Navbar from '../components/Navbar';
 
 
 
 function Home() {
+    // DATA
+    const CAMERA_SHUTTER_COVERS_OPEN_POSITIONS: {[key: string]: (number|JQuery<HTMLElement> | undefined)[]} = {
+        n6: [-120, -200, undefined],
+        n5: [-230, 10, undefined],
+        n4: [-140, 200, undefined],
+        n3: [230, 0, undefined],
+        n2: [230, 0, undefined],
+        n1: [0, -700, undefined]
+    };
+    const CAMERA_SHUTTER_IS_CLOSED: React.MutableRefObject<boolean> = React.useRef(false);
+
+
+
+    // HOOKS
+    React.useEffect(() => {
+        for (const COVER in CAMERA_SHUTTER_COVERS_OPEN_POSITIONS) {
+            CAMERA_SHUTTER_COVERS_OPEN_POSITIONS[COVER][2] = $(`#camera-shutter .cover.${COVER}`).first();
+        }
+    });
+
+
+
+    // HELPERS
+    function toggleCameraShutter() {
+        for (const COVER in CAMERA_SHUTTER_COVERS_OPEN_POSITIONS) {
+            const DATA: (number | JQuery<HTMLElement> | undefined)[] = CAMERA_SHUTTER_COVERS_OPEN_POSITIONS[COVER];
+
+            const COVER_ELEMENT: JQuery<HTMLElement> = DATA[2] as JQuery<HTMLElement>;
+
+            const X: number = DATA[0] as number;
+            const Y: number = DATA[1] as number;
+
+            const X_SPEED: number = 5 // ms
+            const Y_SPEED: number = X_SPEED + 45 // ms
+
+            if (CAMERA_SHUTTER_IS_CLOSED.current === false) {
+                // close shutter
+
+                $({x: X}).animate(
+                    {x: 0},
+                    {
+                        duration: X_SPEED,
+                        step: (now) => {
+                            COVER_ELEMENT.css({transform: `translate(${now}px, ${Y}px)`});
+                        },
+                        complete: () => {
+                            $({y: Y}).animate(
+                                {y: 0},
+                                {
+                                    duration: Y_SPEED,
+                                    step: (now) => {
+                                        COVER_ELEMENT.css({transform: `translate(0px, ${now}px)`});
+                                    },
+                                    complete: () => {
+                                        CAMERA_SHUTTER_IS_CLOSED.current = true;
+                                    }
+                                }
+                            );
+                        }
+                    }
+                );
+            }
+            else {
+                // open shutter
+
+                $({y: 0}).animate(
+                    {y: Y},
+                    {
+                        duration: Y_SPEED,
+                        step: (now) => {
+                            COVER_ELEMENT.css({transform: `translate(0px, ${now}px)`});
+                        },
+                        complete: () => {
+                            $({x: 0}).animate(
+                                {x: X},
+                                {
+                                    duration: X_SPEED,
+                                    step: (now) => {
+                                        COVER_ELEMENT.css({transform: `translate(${now}px, ${Y}px)`});
+                                    },
+                                    complete: () => {
+                                        CAMERA_SHUTTER_IS_CLOSED.current = false;
+                                    }
+                                }
+                            );
+                        }
+                    }
+                );
+            }
+        }
+    };
+
+
+
+    // HTML
     return (
         <div id='home-page'>
             <Navbar />
