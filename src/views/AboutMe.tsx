5a18598d7b2105422a2e28ffd9af8d66a0baf5ed
andre2xu <>
2024-09-08 10:40:38 +0100

    Created a stylesheet for the about me page and added the navbar and footer to it.

--- src/views/AboutMe.tsx
+++ src/views/AboutMe.tsx
@@ -1,6 +1,20 @@
+// shared components
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+
+
 function AboutMe() {
     return (
-        <h1>Test</h1>
+        <div id='about-me-page'>
+            <Navbar />
+
+            <main>
+                About me
+            </main>
+
+            <Footer />
+        </div>
     );
 };
 
