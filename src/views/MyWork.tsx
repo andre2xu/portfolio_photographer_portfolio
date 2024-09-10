cb020dfbe7f8882510bb0c4c972da50d02a4ff1b
andre2xu <>
2024-09-10 11:11:13 +0100

    Created the image grid for the 'My Work' page.

--- src/views/MyWork.tsx
+++ src/views/MyWork.tsx
@@ -6,11 +6,39 @@ import Footer from '../components/Footer';
 
 function MyWork() {
     return (
-        <div id="my-work-page">
+        <div id='my-work-page'>
             <Navbar />
 
             <main>
-                <h1>My Work</h1>
+                <div className='image-column'>
+                    <img src='/images/gallery/business/pexels-javon-swaby-197616-2783873.jpg' alt='Watch' />
+
+                    <img src='/images/gallery/classes/pexels-137666-710744.jpg' alt='Music class' />
+
+                    <img src='/images/gallery/graduation/pexels-hai-nguyen-825252-1699414.jpg' alt='Four college students in graduation' />
+
+                    <img src='/images/gallery/weddings/pexels-rocsana99-948185.jpg' alt='Couple kissing at a wedding while holding a bouquet' />
+                </div>
+
+                <div className='image-column'>
+                    <img src='/images/gallery/modeling/pexels-marleneleppanen-1183266.jpg' alt='Male model wearing a yellow hoodie' />
+
+                    <img src='/images/gallery/weddings/pexels-pixabay-265722.jpg' alt='Married couple laying down on grass' />
+
+                    <img src='/images/gallery/business/pexels-pixabay-279480.jpg' alt='Small and large body spray products' />
+                </div>
+
+                <div className='image-column'>
+                    <img src='/images/gallery/classes/pexels-case-originals-3420517.jpg' alt='Crowd of male students cheering' />
+
+                    <img src='/images/gallery/graduation/pexels-joshua-mcknight-442355-1139319.jpg' alt='Female graduate smiling at camera' />
+
+                    <img src='/images/gallery/modeling/pexels-pixabay-247204.jpg' alt='Two female models posing for camera' />
+
+                    <img src='/images/gallery/classes/pexels-rasy-nak-312175-893924.jpg' alt='Students in a poor country' />
+
+                    <img src='/images/gallery/business/pexels-jose-martin-segura-benites-1422456152-27298415.jpg' alt='White shoes' />
+                </div>
             </main>
 
             <Footer />
