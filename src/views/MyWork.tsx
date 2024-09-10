a7d2eb771563065b9bd486c64fe0ca6e556876c4
andre2xu <>
2024-09-10 17:55:35 +0100

    Made the images in the 'My Work' page focusable and gave them a focus outline.

--- src/views/MyWork.tsx
+++ src/views/MyWork.tsx
@@ -11,33 +11,33 @@ function MyWork() {
 
             <main>
                 <div className='image-column'>
-                    <img src='/images/gallery/business/pexels-javon-swaby-197616-2783873.jpg' alt='Watch' />
+                    <img tabIndex={0} src='/images/gallery/business/pexels-javon-swaby-197616-2783873.jpg' alt='Watch' />
 
-                    <img src='/images/gallery/classes/pexels-137666-710744.jpg' alt='Music class' />
+                    <img tabIndex={0} src='/images/gallery/classes/pexels-137666-710744.jpg' alt='Music class' />
 
-                    <img src='/images/gallery/graduation/pexels-hai-nguyen-825252-1699414.jpg' alt='Four college students in graduation' />
+                    <img tabIndex={0} src='/images/gallery/graduation/pexels-hai-nguyen-825252-1699414.jpg' alt='Four college students in graduation' />
 
-                    <img src='/images/gallery/weddings/pexels-rocsana99-948185.jpg' alt='Couple kissing at a wedding while holding a bouquet' />
+                    <img tabIndex={0} src='/images/gallery/weddings/pexels-rocsana99-948185.jpg' alt='Couple kissing at a wedding while holding a bouquet' />
                 </div>
 
                 <div className='image-column'>
-                    <img src='/images/gallery/modeling/pexels-marleneleppanen-1183266.jpg' alt='Male model wearing a yellow hoodie' />
+                    <img tabIndex={0} src='/images/gallery/modeling/pexels-marleneleppanen-1183266.jpg' alt='Male model wearing a yellow hoodie' />
 
-                    <img src='/images/gallery/weddings/pexels-pixabay-265722.jpg' alt='Married couple laying down on grass' />
+                    <img tabIndex={0} src='/images/gallery/weddings/pexels-pixabay-265722.jpg' alt='Married couple laying down on grass' />
 
-                    <img src='/images/gallery/business/pexels-pixabay-279480.jpg' alt='Small and large body spray products' />
+                    <img tabIndex={0} src='/images/gallery/business/pexels-pixabay-279480.jpg' alt='Small and large body spray products' />
                 </div>
 
                 <div className='image-column'>
-                    <img src='/images/gallery/classes/pexels-case-originals-3420517.jpg' alt='Crowd of male students cheering' />
+                    <img tabIndex={0} src='/images/gallery/classes/pexels-case-originals-3420517.jpg' alt='Crowd of male students cheering' />
 
-                    <img src='/images/gallery/graduation/pexels-joshua-mcknight-442355-1139319.jpg' alt='Female graduate smiling at camera' />
+                    <img tabIndex={0} src='/images/gallery/graduation/pexels-joshua-mcknight-442355-1139319.jpg' alt='Female graduate smiling at camera' />
 
-                    <img src='/images/gallery/modeling/pexels-pixabay-247204.jpg' alt='Two female models posing for camera' />
+                    <img tabIndex={0} src='/images/gallery/modeling/pexels-pixabay-247204.jpg' alt='Two female models posing for camera' />
 
-                    <img src='/images/gallery/classes/pexels-rasy-nak-312175-893924.jpg' alt='Students in a poor country' />
+                    <img tabIndex={0} src='/images/gallery/classes/pexels-rasy-nak-312175-893924.jpg' alt='Students in a poor country' />
 
-                    <img src='/images/gallery/business/pexels-jose-martin-segura-benites-1422456152-27298415.jpg' alt='White shoes' />
+                    <img tabIndex={0} src='/images/gallery/business/pexels-jose-martin-segura-benites-1422456152-27298415.jpg' alt='White shoes' />
                 </div>
             </main>
 
